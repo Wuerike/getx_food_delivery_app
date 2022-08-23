b0a8c85d17d16cb7ce8322fc9beffeb481a9266a
Wuerike <wuerike.henri@gmail.com>
2022-08-23 10:03:05 -0300

    chore: Add item description card

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+import '../widgets/icon_and_text_widget.dart';
 
 class FoodPageBody extends StatefulWidget {
   FoodPageBody({Key? key}) : super(key: key);
@@ -37,6 +42,77 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             image: const DecorationImage(
               fit: BoxFit.cover,
               image: AssetImage("assets/image/food0.png"),
+            ),
+          ),
+        ),
+        // White card
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.shadowColor,
+                  blurRadius: 5.0,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Name
+                  BigText(text: "Chinese Side"),
+                  const SizedBox(height: 10),
+                  // Rating info
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10),
+                      SmallText(text: "1234 comments"),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  // Status info
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconAndTextWidget(
+                        text: "Normal",
+                        icon: Icons.circle_sharp,
+                        iconColor: AppColors.iconColor1,
+                      ),
+                      IconAndTextWidget(
+                        text: "1.7km",
+                        icon: Icons.location_on,
+                        iconColor: AppColors.mainColor,
+                      ),
+                      IconAndTextWidget(
+                        text: "32min",
+                        icon: Icons.access_time_rounded,
+                        iconColor: AppColors.iconColor2,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
