99c01a0177da46e5abb8ba5bca2b92e6b3c78464
Wuerike <wuerike.henri@gmail.com>
2022-08-23 22:39:13 -0300

    feat: Add app dimensions util class

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/home/main_food_page.dart';
 import 'package:food_delivery/utils/app_scroll_behavior.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       scrollBehavior: AppScrollBehavior(),
