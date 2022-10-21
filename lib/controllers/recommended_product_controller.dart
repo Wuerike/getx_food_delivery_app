724e51f0d21a6dcbd659c93a719ec6bfe05636aa
Wuerike <wuerike.henri@gmail.com>
2022-10-21 20:06:38 -0300

    chore: Add itens to cart through recommended product controller

--- lib/controllers/recommended_product_controller.dart
+++ lib/controllers/recommended_product_controller.dart
@@ -1,17 +1,31 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:food_delivery/models/product.dart';
 import 'package:get/get.dart';
 
+import 'package:food_delivery/controllers/cart_controller.dart';
 import 'package:food_delivery/data/repository/recommended_product_repository.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
 
 class RecommendedProductController extends GetxController {
   final RecommendedProductRepository recommendedProductRepository;
+  final CartController cartController;
 
   RecommendedProductController({
     required this.recommendedProductRepository,
+    required this.cartController,
   });
 
   List<dynamic> _recommendedProductList = [];
   List<dynamic> get recommendedProductList => _recommendedProductList;
+
+  int _quantity = 0;
+  int get quantity => _quantity;
+
+  int get totalQuantity {
+    return cartController.totalQuantity;
+  }
 
   Future<void> getRecommendedProductList() async {
     _recommendedProductList = [];
@@ -25,5 +39,51 @@ class RecommendedProductController extends GetxController {
       //TO-DO: Handle exceptions
       print(e.toString());
     }
+  }
+
+  void setDefaultQuantity(Product product) {
+    _quantity = 0;
+  }
+
+  void setQuantity(bool isIncrement) {
+    if (isIncrement) {
+      _quantity = _quantity + 1;
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
+    }
+
+    update();
+  }
+
+  int checkQuantity(int value) {
+    if (value < 0) {
+      Get.snackbar(
+        "Item count:",
+        "Can't be less then 0.",
+        margin: EdgeInsets.all(Dimensions.height10),
+        animationDuration: Duration(milliseconds: 500),
+        backgroundColor: Colors.white.withOpacity(0.8),
+      );
+      return 0;
+    }
+
+    return value;
+  }
+
+  bool addItemToCart(Product product) {
+    if (_quantity < 1) {
+      Get.snackbar(
+        "Item count:",
+        "Can't add 0 itens to the cart",
+        margin: EdgeInsets.all(Dimensions.height10),
+        animationDuration: Duration(milliseconds: 500),
+        backgroundColor: Colors.white.withOpacity(0.8),
+      );
+      return false;
+    }
+    cartController.addItem(product, _quantity);
+    _quantity = 0;
+    update();
+    return true;
   }
 }
